71cbfe97359bb1592100394698dfe5511c42bd39
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-04-29 17:23:07 +0300

    other mouse events, computed properties, computing list,attribute-binding dynamic-classes toggling data and events

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
+      url: "devmaxwel.github.io/portfolio/",
       name: "Maxwel Ochieng",
-      book: "The Easier way to Learn React",
+      books: [
+        {
+          title: "The A Student",
+          year: 1990,
+          author: "Maxwel",
+          img: "assets/1.png",
+          isFav:true
+        },
+        {
+          title: "The Do's and Dont's of Developer",
+          year: 1990,
+          author: "Maxwel",
+          img: "assets/2.png",
+          isFav: false,
+        },
+        {
+          title: "JavaScript Cheatsheet",
+          year: 1990,
+          author: "Maxwel",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
       age: 24,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
@@ -17,9 +42,28 @@ const app = Vue.createApp({
       console.log("Title Changed");
       this.book = "Next JS Cheat sheet";
     },
-    toggleShowBooks(){
-        this.showBooks = !this.showBooks
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        return console.log(data);
+      }
+    },
+    handleMove(e) {
+      (this.x = e.offsetX), (this.y = e.offsetY);
+    },
+    handleColor(book){
+       book.isFav = !book.isFav
     }
   },
+  computed:{
+      filteredBooks(){
+          return this.books.filter((book) =>{
+              return book.isFav
+          })
+      }
+  }
 });
 app.mount("#app");
